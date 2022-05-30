33e2726d4543ae16a3b57cf22c30d7862da9facf
Sayantan <sayantan.dps@gmail.com>
2022-05-30 20:29:50 +0530

    fixes

---
+++ frontend/build/precache-manifest.87decd0b18697ef5ce3338cae129aef9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac8a4c225282524c72732fd9d55b58a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ec6fae0e7423a0a6839",
+    "url": "/static/css/main.233cbc38.chunk.css"
+  },
+  {
+    "revision": "c23d5974e2945618490f",
+    "url": "/static/js/2.89a5c1db.chunk.js"
+  },
+  {
+    "revision": "6ec6fae0e7423a0a6839",
+    "url": "/static/js/main.6cf35ad8.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,9 @@
 const main = async () => {
   const [deployer] = await hre.ethers.getSigners();
-  const accountBalanace = await deployer.getBalance();
+  const accountBalance = await deployer.getBalance();
 
   console.log("Deploying contract with account: ", deployer.address);
-  console.log("Account balance: ", accountBalanace.toString());
+  console.log("Account balance: ", accountBalance.toString());
 
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
